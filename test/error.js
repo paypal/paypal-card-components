3c0b23f3fbce10b589572d3728ea01842bbff00a
Blade Barringer <blade.barringer@getbraintree.com>
2018-04-09 17:08:39 +0000

    Initial work to add Hosted Fields

--- test/error.js
+++ test/error.js
@@ -3,6 +3,7 @@
 import '../src/index';
 import assert from 'assert';
 
+import btClient from 'braintree-web/client';
 import hostedFields from 'braintree-web/hosted-fields';
 import td from 'testdouble/dist/testdouble';
 
@@ -12,28 +13,81 @@ function rejectIfResolves () {
 
 describe('Error cases', () => {
     let client;
-    let create;
+    let hostedFieldsCreate;
+    let btClientCreate;
     let fakeHostedFieldsInstance;
+    let fakeBtClient;
+    let renderOptions;
 
     beforeEach(() => {
-        client = window.paypal.client();
-        fakeHostedFieldsInstance = td.object([ 'tokenize' ]);
-        create = td.replace(hostedFields, 'create');
+        client = window.paypal.client({
+            env:  'production',
+            auth: {
+                production: 'PROD'
+            }
+        });
+        btClientCreate = td.replace(btClient, 'create');
 
-        td.when(create(td.matchers.isA(Object))).thenResolve(fakeHostedFieldsInstance);
+        fakeHostedFieldsInstance = td.object([ 'tokenize' ]);
+        hostedFieldsCreate = td.replace(hostedFields, 'create');
+
+        fakeBtClient = {
+            getConfiguration: (conf) => conf
+        };
+
+        td.when(hostedFieldsCreate(td.matchers.isA(Object))).thenResolve(fakeHostedFieldsInstance);
+        td.when(btClientCreate(td.matchers.isA(Object))).thenResolve(fakeBtClient);
+        renderOptions = {
+            payment: () => {
+                return 'order-id';
+            }
+        };
     });
 
     afterEach(() => {
         td.reset();
     });
 
+    it('rejects if no auth is provided', () => {
+        client = window.paypal.client({
+            env:  'production'
+        });
+
+        return client.HostedFields.render(renderOptions).then(rejectIfResolves).catch((err) => {
+            assert.equal(err.message, 'Invalid auth encountred. Check how you are creating your client.');
+        });
+    });
+
+    it('rejects if no payments function is provided', () => {
+        client = window.paypal.client({
+            env:  'production'
+        });
+
+        return client.HostedFields.render(renderOptions).then(rejectIfResolves).catch((err) => {
+            assert.equal(err.message, 'Invalid auth encountred. Check how you are creating your client.');
+        });
+    });
+
+    it('rejects with an error if braintree-web.client errors out', () => {
+        let error = new Error('Some BT Web client Error');
+
+        td.when(btClientCreate(td.matchers.isA(Object))).thenReject(error);
+
+        return client.HostedFields.render(renderOptions).then(rejectIfResolves).catch((err) => {
+            td.verify(hostedFieldsCreate(td.matchers.anything()), {
+                times: 0
+            });
+            assert.equal(err.message, 'Some BT Web client Error');
+        });
+    });
+
     it('rejects with an error if braintree-web.hosted-fields errors out', () => {
         let error = new Error('Some BT Web Error');
 
-        td.when(create(td.matchers.isA(Object))).thenReject(error);
+        td.when(hostedFieldsCreate(td.matchers.isA(Object))).thenReject(error);
 
-        return client.HostedFields.render({}).then(rejectIfResolves).catch((err) => {
-            assert.equal(err.message, 'Something went wrong.');
+        return client.HostedFields.render(renderOptions).then(rejectIfResolves).catch((err) => {
+            assert.equal(err.message, 'Some BT Web Error');
         });
     });
 });
--- test/happy.js
+++ test/happy.js
@@ -2,41 +2,74 @@
 
 import '../src/index';
 
+import btClient from 'braintree-web/client';
 import hostedFields from 'braintree-web/hosted-fields';
 import td from 'testdouble/dist/testdouble';
 
 describe('Happy cases', () => {
     let client;
-    let create;
+    let hostedFieldsCreate;
+    let btClientCreate;
     let fakeHostedFieldsInstance;
+    let fakeBtClient;
 
     beforeEach(() => {
-        client = window.paypal.client();
-        fakeHostedFieldsInstance = td.object([ 'tokenize' ]);
-        create = td.replace(hostedFields, 'create');
+        client = window.paypal.client({
+            env:  'production',
+            auth: {
+                production: 'PROD'
+            }
+        });
+        btClientCreate = td.replace(btClient, 'create');
 
-        td.when(create(td.matchers.isA(Object))).thenResolve(fakeHostedFieldsInstance);
+        fakeHostedFieldsInstance = td.object([ 'tokenize' ]);
+        hostedFieldsCreate = td.replace(hostedFields, 'create');
+
+        fakeBtClient = {
+            getConfiguration: (conf) => conf
+        };
+
+        td.when(hostedFieldsCreate(td.matchers.isA(Object))).thenResolve(fakeHostedFieldsInstance);
+        td.when(btClientCreate(td.matchers.isA(Object))).thenResolve(fakeBtClient);
     });
 
     afterEach(() => {
         td.reset();
     });
 
-    it('Should create a Hosted Fields instance', () => {
-        let options = {};
+    it('Should create a Braintree client and Hosted Fields instance with configuration', () => {
+        let renderOptions = {
+            payment: () => {
+                return 'order-id';
+            }
+        };
 
-        return client.HostedFields.render(options).then(() => {
-            td.verify(create(options));
+        return client.HostedFields.render(renderOptions).then(() => {
+            td.verify(btClientCreate({
+                authorization: 'PROD',
+                configuration: td.matchers.isA(Object)
+            }));
+            td.verify(hostedFieldsCreate({
+                client:      fakeBtClient,
+                paymentsSdk: true
+            }));
         });
     });
 
     it('Resolves with an object that can tokenize', () => {
-        return client.HostedFields.render({}).then((handler) => {
+        let renderOptions = {
+            payment: () => {
+                return 'order-id';
+            }
+        };
+        return client.HostedFields.render(renderOptions).then((handler) => {
             let options = {};
 
-            handler.submit(options);
-
-            td.verify(fakeHostedFieldsInstance.tokenize(options));
+            return handler.submit(options);
+        }).then(() => {
+            td.verify(fakeHostedFieldsInstance.tokenize({
+                orderId: 'order-id'
+            }));
         });
     });
 });
