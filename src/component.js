cbd9803d641b7d3052660d9eddc68a442621d8b4
Daniel Brain <dbrain@paypal.com>
2018-09-07 14:09:31 -0700

    Export public interface using ES6 semantics

--- src/component.js
+++ src/component.js
@@ -1,6 +1,6 @@
 /* @flow */
 
-import { attach } from 'paypal-braintree-web-client/src';
+import { getClientToken } from 'paypal-braintree-web-client/src';
 
 // toodoo unvendor this when braintree-web is updated
 import btClient from '../vendor/braintree-web/client';
@@ -38,79 +38,66 @@ function createSubmitHandler (hostedFieldsInstance, orderIdFunction) : Function 
   };
 }
 
-attach('hosted-fields', ({ clientOptions }) => {
-  let { auth } = clientOptions;
+export let HostedFields = {
+  render(options, buttonSelector) : Promise<HostedFieldsHandler> {
 
-  // toodoo - revert change below when config is being passed correctly
-  let configuration = (typeof __hosted_fields__ !== 'undefined') ? __hosted_fields__.serverConfig : TESTING_CONFIGURATION;
-  configuration.assetsUrl = TESTING_CONFIGURATION.assetsUrl;
-  if (!configuration.card && configuration.paypalMerchantConfiguration && configuration.paypalMerchantConfiguration.creditCard) {
-    configuration.card = configuration.paypalMerchantConfiguration.creditCard;
-  } else {
-    //configuration.card = TESTING_CONFIGURATION.card;
-  }
-  console.log("Using config");
-  console.log(configuration);
+    // toodoo - revert change below when config is being passed correctly
+    let configuration = (typeof __hosted_fields__ !== 'undefined') ? __hosted_fields__.serverConfig : TESTING_CONFIGURATION;
+    configuration.assetsUrl = TESTING_CONFIGURATION.assetsUrl;
+    if (!configuration.card && configuration.paypalMerchantConfiguration && configuration.paypalMerchantConfiguration.creditCard) {
+      configuration.card = configuration.paypalMerchantConfiguration.creditCard;
+    } else {
+      // configuration.card = TESTING_CONFIGURATION.card;
+    }
+    console.log('Using config');
+    console.log(configuration);
 
-  let env = __ENV__;
+    let clientToken = getClientToken();
 
-  let correlationId = '';
-  configuration.correlationId = correlationId;
+    let correlationId = __CORRELATION_ID__;
+    configuration.correlationId = correlationId;
 
-  console.log('CID: ' + correlationId);
-  return {
-    HostedFields: {
-      render(options, buttonSelector) : Promise<HostedFieldsHandler> {
-        if (!auth || !auth[env]) {
-          return Promise.reject(new Error('Invalid auth encountred. Check how you are creating your client.'));
-        }
+    let orderIdFunction = () => {
+      return Promise.resolve().then(() => {
+        return options.payment();
+      });
+    };
+    let button;
 
-        let orderIdFunction = () => {
-          return Promise.resolve().then(() => {
-            return options.payment();
+    if (buttonSelector && options.onAuthorize) {
+      button = document.querySelector(buttonSelector);
+      if (!button) {
+        return Promise.reject(new Error(`Could not find selector \`${ buttonSelector }\` on the page`));
+      }
+    }
+
+    return btClient.create({
+      authorization: clientToken,
+      paymentsSdk:   true,
+      configuration
+    }).then((btClientInstance) => {
+      let hostedFieldsCreateOptions = JSON.parse(JSON.stringify(options));
+
+      hostedFieldsCreateOptions.paymentsSdk = true;
+      hostedFieldsCreateOptions.client = btClientInstance;
+      return hostedFields.create(hostedFieldsCreateOptions);
+    }).then((hostedFieldsInstance) => {
+      hostedFieldsInstance.submit = createSubmitHandler(hostedFieldsInstance, orderIdFunction);
+
+      if (button) {
+        button.addEventListener('click', () => {
+          hostedFieldsInstance.submit().then((payload) => {
+            return options.onAuthorize(payload);
+          }).catch((err) => {
+
+            if (options.onError) {
+              options.onError(err);
+            }
           });
-        };
-        let button;
-
-        if (buttonSelector && options.onAuthorize) {
-          button = document.querySelector(buttonSelector);
-          if (!button) {
-            return Promise.reject(new Error(`Could not find selector \`${ buttonSelector }\` on the page`));
-          }
-        }
-
-        return btClient.create({
-          authorization: auth[env],
-          paymentsSdk:   true,
-          configuration
-        }).then((btClientInstance) => {
-          let hostedFieldsCreateOptions = JSON.parse(JSON.stringify(options));
-
-          hostedFieldsCreateOptions.paymentsSdk = true;
-          hostedFieldsCreateOptions.client = btClientInstance;
-          return hostedFields.create(hostedFieldsCreateOptions);
-        }).then((hostedFieldsInstance) => {
-          hostedFieldsInstance.submit = createSubmitHandler(hostedFieldsInstance, orderIdFunction);
-
-          if (button) {
-            button.addEventListener('click', () => {
-              hostedFieldsInstance.submit().then((payload) => {
-                return options.onAuthorize(payload);
-              }).catch((err) => {
-
-                if (options.onError) {
-                  options.onError(err);
-                }
-              });
-            });
-          }
-
-          return hostedFieldsInstance;
         });
       }
-    },
 
-    HOSTED_FIELDS_CONSTANTS: {
-    }
-  };
-});
+      return hostedFieldsInstance;
+    });
+  }
+};
