805654f6a9f1d30ba04808bc7559d3ab94c1972d
Daniel Brain <dbrain@paypal.com>
2018-11-15 11:12:12 -0800

    Type fixes

--- src/types.js
+++ src/types.js
@@ -1,6 +1,5 @@
 /* @flow */
 
-import type { FundingEligibilityType } from 'paypal-braintree-web-client/src';
 import { ZalgoPromise } from 'zalgo-promise/src';
 
 type SubmitFunctionType = (options? : Object) => ZalgoPromise<Object>;
@@ -8,6 +7,39 @@ type SubmitFunctionType = (options? : Object) => ZalgoPromise<Object>;
 export type HostedFieldsHandler = {
     submit : SubmitFunctionType
 };
+
+export type FundingEligibilityType = {|
+    card : {
+        eligible : boolean,
+        branded : boolean,
+        vendors : {
+            visa : {
+                eligible : boolean
+            },
+            mastercard : {
+                eligible : boolean
+            },
+            amex : {
+                eligible : boolean
+            },
+            discover : {
+                eligible : boolean
+            },
+            hiper : {
+                eligible : boolean
+            },
+            elo : {
+                eligible : boolean
+            },
+            jcb : {
+                eligible : boolean
+            },
+            cup : {
+                eligible : boolean
+            }
+        }
+    }
+|};
 
 export type HostedFieldsServerConfigType = {
     fundingEligibility : FundingEligibilityType
