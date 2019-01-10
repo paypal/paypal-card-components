25792acb79013186494146d4cd2b570aea8a30c4
Daniel Brain <dbrain@paypal.com>
2019-01-10 10:50:33 -0800

    Upgrade to zoid 8

--- test/contingency-flow.js
+++ test/contingency-flow.js
@@ -10,10 +10,15 @@ import contingencyFlow from '../src/contingency-flow';
 import rejectIfResolves from './utils/reject-if-resolves';
 
 describe('contingency-flow', () => {
+  let fakeContingencyInit;
   let fakeContingencyComponentRender;
 
   beforeEach(() => {
-    fakeContingencyComponentRender = td.replace(contingencyFlow.ContingencyComponent, 'render');
+    fakeContingencyInit = td.replace(contingencyFlow.contingency, 'Component');
+    fakeContingencyComponentRender = td.func();
+    td.when(fakeContingencyInit(), { ignoreExtraArgs: true }).thenReturn({
+      render: fakeContingencyComponentRender
+    });
   });
 
   afterEach(() => {
@@ -23,19 +28,21 @@ describe('contingency-flow', () => {
   it('renders a zoid component', () => {
     contingencyFlow.start('https://example.com?cart_id=abc123&action=action&xcomponent=1&flow=contingency');
 
-    td.verify(fakeContingencyComponentRender({
+    td.verify(fakeContingencyInit({
       action:              'action',
       xcomponent:           '1',
       flow:                'contingency',
       cart_id:             'abc123',
       onContingencyResult: td.matchers.isA(Function),
       onError:             td.matchers.isA(Function)
-    }, document.body));
+    }));
+
+    td.verify(fakeContingencyComponentRender(document.body));
   });
 
   it('rejects when contingency returns an error object with code and description', () => {
     let promise = contingencyFlow.start('https://example.com?cart_id=abc123&action=contingency&xcomponent=1&flow=contingency');
-    let onContingencyResult = td.explain(fakeContingencyComponentRender).calls[0].args[0]
+    let onContingencyResult = td.explain(fakeContingencyInit).calls[0].args[0]
       .onContingencyResult;
     let error = {
       code:        42,
@@ -51,7 +58,7 @@ describe('contingency-flow', () => {
 
   it('rejects when contingency returns an error object with description only', () => {
     let promise = contingencyFlow.start('https://example.com?cart_id=abc123&action=contingency&xcomponent=1&flow=contingency');
-    let onContingencyResult = td.explain(fakeContingencyComponentRender).calls[0].args[0]
+    let onContingencyResult = td.explain(fakeContingencyInit).calls[0].args[0]
       .onContingencyResult;
     let error = {
       description: 'The error of life'
@@ -66,7 +73,7 @@ describe('contingency-flow', () => {
 
   it('resolves when contingency is successful', () => {
     let promise = contingencyFlow.start('https://example.com?cart_id=abc123&action=contingency&xcomponent=1&flow=contingency');
-    let onContingencyResult = td.explain(fakeContingencyComponentRender).calls[0].args[0]
+    let onContingencyResult = td.explain(fakeContingencyInit).calls[0].args[0]
       .onContingencyResult;
 
     onContingencyResult(null, {
@@ -81,7 +88,7 @@ describe('contingency-flow', () => {
   it('rejects when onError is called with an error', () => {
     let randomError = new Error('spooky');
     let promise = contingencyFlow.start('https://example.com?cart_id=abc123&action=contingency&xcomponent=1&flow=contingency');
-    let onError = td.explain(fakeContingencyComponentRender).calls[0].args[0]
+    let onError = td.explain(fakeContingencyInit).calls[0].args[0]
       .onError;
 
     onError(randomError);
