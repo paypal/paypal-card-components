5456de23311a32ef4a09df6e801c575bf46047df
Rohan Baddam <rbaddam@paypalcorp.com>
2019-10-22 11:39:03 -0700

    Changed paymentsource to card

--- src/component.js
+++ src/component.js
@@ -91,7 +91,7 @@ function createSubmitHandler (hostedFieldsInstance, orderIdFunction) : Function 
           liabilityShifted,
           authenticationStatus: payload.status,
           authenticationReason: payload.authentication_status_reason,
-          paymentSource:        payload.payment_source,
+          card:                 payload.payment_source.card,
           orderId
         };
       }).catch((err) => {
