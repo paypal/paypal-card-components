cc4dbd17687edfc7131be3356646941059545523
Westin Wrzesinski <westeezy@users.noreply.github.com>
2022-04-11 06:49:10 -0700

    chore: move devDependencies to @krakenjs scope (#65)

--- test/.eslintrc.js
+++ test/.eslintrc.js
@@ -1,12 +1,13 @@
 /* @flow */
 
+// eslint-disable-next-line import/no-commonjs
 module.exports = {
   extends: '../.eslintrc.js',
 
   rules: {
-    'import/no-nodejs-modules': 'off',
-    'import/export': 'off',
+    'import/no-nodejs-modules':    'off',
+    'import/export':               'off',
     'import/no-unassigned-import': 'off',
-    'no-console': 'warn'
+    'no-console':                  'warn'
   }
 };
